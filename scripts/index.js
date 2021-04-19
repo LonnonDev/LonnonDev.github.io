35c64f4a76789cfcc7a976b2de2207b8b1924056
LonnonjamesD <LonnonjamesD@gmail.com>
2021-04-19 14:42:09 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,10 @@
 var pageContent = document.getElementById("sidebar").innerHTML; 
 sessionStorage.setItem("sidebar", pageContent);
-if (window.console) console = { 
-    log: function(){
-        var output='',
-            console=document.getElementById('log');
-        for (var i=0;i<arguments.length;i++) {
-            output+=arguments[i]+' ';
-        }
-        console.innerText+=output+"\n";
-    }
-};
-window.onload = function(){
-    var refButton = document.getElementById("btnButton");
 
-    refButton.onclick = function() {
-    alert('I am clicked!');
-    }
+window.onload = function(){
+	var refButton = document.getElementById("btnButton");
+
+	refButton.onclick = function() {
+		alert('I am clicked!');
+	}
 };
