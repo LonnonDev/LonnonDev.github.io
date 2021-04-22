36dcbda784a24bd0fcb07bdbc2d8498584a442db
LonnonjamesD <LonnonjamesD@gmail.com>
2021-04-22 00:05:33 -0400

    Blog Update

--- js/blogs.js
+++ js/blogs.js
@@ -1,3 +1,13 @@
 $(".blog1").load("/blogs/blog1.html")
 $(".blog2").load("/blogs/blog2.html")
 $(".blog3").load("/blogs/blog3.html")
+
+function ShowBlog(blognum) {
+	var Blogs = document.querySelectorAll('[id=blog]')
+	var BlogsLength = Blogs.length
+	for (i = 1; i < BlogsLength/2+1; i++) {
+		var BlogPage = document.getElementsByClassName("blog".concat(parseInt(i)))[0]
+		BlogPage.style.display = "none"
+	}
+	document.getElementsByClassName("blog".concat(blognum))[0].style.display = "contents"
+}
